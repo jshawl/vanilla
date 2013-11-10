e2815bc0637567f869c197214e261b021c56a050
Jesse Shawl <jesse@jshawl.com>
2013-11-10 18:44:12 -0500

    added compass to scss workflow

---
+++ scss/config.rb
@@ -0,0 +1,17 @@
+debug = :true
+css_dir = "../css"
+sass_dir = "./"
+
+require 'autoprefixer-rails'
+require 'csso'
+
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  File.open(file,'w') do |io|
+    if debug == :true
+      io << AutoprefixerRails.compile(css)
+    else
+      io << Csso.optimize(AutoprefixerRails.compile(css))
+    end
+  end
+end
