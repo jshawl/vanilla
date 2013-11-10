43f176d89b55880d309d6cbdb27a5b0adcfbbcfa
Jesse Shawl <jesse@jshawl.com>
2013-11-10 17:27:42 -0500

    updated grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,17 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      dist: {
-        options:{
-          style:'compressed' 
-        },
-        files: {
-          'css/style.css' : 'scss/style.scss'
-        }
-      }
-    },
-    autoprefixer:{
-      dist:{
-        files:{
-          'css/style.css':'css/style.css'
-        }
-      }
-    },
-    csso: {
-      dist: {
-        files: {
-          'css/style.css': ['css/style.css']
-        }
-      }
-    },
-    watch: {
-      css: {
-        files: 'scss/*.scss',
-        tasks: ['sass','autoprefixer','csso'] 
+    uglify: {
+      options: {
+	// the banner is inserted at the top of the output
+	banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      build: {
+	src: 'js/index.js',
+	dest: 'js/index.min.js'
       }
     }
   });
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-csso');
-  grunt.registerTask('default',['watch']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default',['uglify']);
 }
